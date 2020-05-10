86580b204369611b3e7d9293879830d377fbc491
Buck <buck@buckrozelle.com>
2020-05-10 15:08:01 -0400

    Add About View.

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,44 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Buck Rozelle on 5/10/20.
+//  Copyright © 2020 buckrozelledotcomLLC. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class AboutViewController: UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let url = Bundle.main.url(forResource: "BullsEye",
+                                     withExtension: "html") {
+        
+        let request = URLRequest(url: url)
+        webView.load(request)
+
+        // Do any additional setup after loading the view.
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
