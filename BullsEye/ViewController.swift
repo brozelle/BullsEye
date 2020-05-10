0c0d98d46eb7e622d9fcd2158ed13eadd5da048c
Buck <buck@buckrozelle.com>
2020-05-10 12:25:07 -0400

    Add slider, label, and buttons to UI.

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,7 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var currentValue = 50
+    
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        
+        //round slider.value and it into currentValue
+        currentValue = lroundf(slider.value)
+        print("The value of the slider is now: \(slider.value)")
+    }
+    
+    /*
+     User presses Hit Me! button.
+ */
+    @IBAction func showAlert() {
+        let message = "The value of the slider is: \(currentValue)"
+        
+        let alert = UIAlertController(title: "Hello, World",
+                                      message: message,
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default,
+                                   handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
