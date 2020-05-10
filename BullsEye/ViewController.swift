7aceb839c6c49e97f90238ae6c40853aee4e0fe2
Buck <buck@buckrozelle.com>
2020-05-10 13:23:57 -0400

    Added score, round, and update label functions.

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,9 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var targetValue = 0
     var currentValue = 50
+    var score = 0
+    var round = 0
     
+    //round outlet
+    @IBOutlet weak var roundLabel: UILabel!
     
+    //slider.value goes into slider as instance var.
+    @IBOutlet weak var slider: UISlider!
+    
+    //targetValue outlet
+    @IBOutlet weak var targetLabel: UILabel!
+    
+    //score outlet
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    //slider outlet
     @IBAction func sliderMoved(_ slider: UISlider) {
         
         //round slider.value and it into currentValue
@@ -23,7 +38,12 @@ class ViewController: UIViewController {
      User presses Hit Me! button.
  */
     @IBAction func showAlert() {
-        let message = "The value of the slider is: \(currentValue)"
+        let difference = abs(targetValue - currentValue)
+        let points = 100 - difference
+        
+        score += points
+        
+        let message = "The value of the slider is: \(currentValue)" + "\nThe targe value is: \(targetValue)" + "\nThe difference is: \(difference)"
         
         let alert = UIAlertController(title: "Hello, World",
                                       message: message,
@@ -32,11 +52,39 @@ class ViewController: UIViewController {
                                    handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        
+        startNewRound()
     }
-    
+    /*
+     When the ViewController loads:
+     startNewRound is called.
+ */
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        startNewRound()
+    }
+    
+    /*
+     User presses the startOver button and:
+     slider.value is rounded off and stored in currentValue(typed as a Float.)
+     targetValue is assigned a new random number between 1 and 100.
+ */
+    
+    func startNewRound() {
+        round += 1
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    
+    /*
+     Updates...
+ */
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
     }
 
 
